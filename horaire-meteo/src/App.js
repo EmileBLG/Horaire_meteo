709d495ea17e053a0d1dee64e1b815593ab64950
EmileBLG <bele1103@usherbrooke.ca>
2023-12-02 15:21:08 -0500

    a

--- horaire-meteo/src/App.js
+++ horaire-meteo/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <div className='Meteo'>
         <Condition_actuelle />
+        <Condition_actuelle />
       </div>
       <div className='Horaire'>
         <p>Div qui contient l'horaire</p>
